1df8d7c34e5916e9ce68305e2c9104fc1a7fbc07
MarciaAguiar1701 <marciaatoa@gmail.com>
2024-05-13 17:16:45 -0300

    Aula-final-variaveis

---
+++ tipos-variaveis/src/Tiposvariaveis.java
@@ -0,0 +1,21 @@
+public class Tiposvariaveis {
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+        
+        String meuNome = "Marcia Aguiar";
+
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; 
+
+        int numero = 5;
+        numero = 10;
+
+		System.out.print(numero);
+
+    }
+}
